e4ab7899b285af5a36ae6e654397252867d11936
vincent-pli <justdoit.pli@gmail.com>
2023-05-30 21:38:47 +0800

    refresh image

--- launchModel/dockerImp.py
+++ launchModel/dockerImp.py
@@ -16,7 +16,7 @@ class DockerLauncher(BaseModel):
 
         client = docker.from_env()
         container = client.containers.run(
-            "docker.io/vincentpli/mode-base:v0.1", detach=True, environment=environments, ports=ports, volumes = volumn)
+            "docker.io/vincentpli/mode-base:v0.2", detach=True, environment=environments, ports=ports, volumes = volumn)
         container.reload()
         print(container.ports)
         return container.ports
